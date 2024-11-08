00c18359303ead1b2a5cb2fcfcc271f67e029030
AkshayMobileApplicationEngineer <akshaykumarprajapati@hotmail.com>
2024-11-08 21:58:17 +0530

    first

---
+++ index.js
@@ -0,0 +1,9 @@
+var http = require("http");
+var port = 8080;
+
+http.createServer(function (request, response) {
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   response.end('Hello World\n');
+}).listen(port);
+
+console.log("Server running at http://localhost:" + port + "/");
